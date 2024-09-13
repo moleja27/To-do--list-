b5c2377abd57bb9f9aee88a1fd7caa9d7792eccc
Alejandra <monicsalejandra@gmail.com>
2024-09-13 21:06:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import './App.css'; // Esto puedes mantenerlo si quieres usar estilos básicos
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Mi Aplicación en Blanco</h1>
+      {/* Puedes empezar a construir aquí */}
+    </div>
+  );
+}
+
+export default App;
